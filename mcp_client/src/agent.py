b9b357ef8c9cfa3b05319013c3b402535c7d92b1
renya <renk2510@gmail.com>
2025-07-23 00:56:09 +0000

    strands sample code

---
+++ mcp_client/src/agent.py
@@ -0,0 +1,27 @@
+import os
+
+from dotenv import load_dotenv
+from mcp.client.streamable_http import streamablehttp_client
+from strands import Agent
+from strands.tools.mcp import MCPClient
+
+load_dotenv()
+
+AGENT_ARN = os.getenv("AGENT_ARN")
+BEARER_TOKEN = os.getenv("COGNITO_ACCESS_TOKEN")
+region = "us-west-2"
+
+encoded_arn = AGENT_ARN.replace(":", "%3A").replace("/", "%2F")
+MCP_SERVER_ENDPOINT = f"https://bedrock-agentcore.{region}.amazonaws.com/runtimes/{encoded_arn}/invocations?qualifier=DEFAULT"
+
+mcp_client = MCPClient(
+    lambda: streamablehttp_client(
+        MCP_SERVER_ENDPOINT, headers={"Authorization": f"Bearer {BEARER_TOKEN}"}
+    )
+)
+
+with mcp_client:
+    tools = mcp_client.list_tools_sync()
+    agent = Agent(tools=tools)
+
+    agent("私は ren8k です。挨拶してください")
